5b1b531cb347b962dade3b07bfd9b2e3365ab304
Eduard Gerlits <eduard.gerlits@yahoo.de>
2017-08-19 19:32:18 +0200

    Funktionalitäten hinzugefügt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-$("body").css("background","green");
+// Erledigte Aufgaben streichen
+$(".container ul").on("click", "li",function(){
+	$(this).toggleClass("finished");
+});
+
+// Neue Aufgaben anlegen
+$("input[type='text']").on("keypress", function(event){
+	if(event.which==13){
+		var content = $("input[type='text']").val();
+		$("input[type='text']").val("");
+		$("ul").append("<li style='display: none'><span>X</span> " + content + "</li>");
+		$("ul li:last-child").fadeIn();
+	}
+});
+
+// Aufgaben löschen
+$("ul").on("click","span", function(event){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
